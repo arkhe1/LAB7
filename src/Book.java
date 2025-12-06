6834b2adac41bc5d5896deb1c0f6dd0921e98111
Umutberk <umutberksanli@gmail.com>
2025-12-07 01:40:54 +0300

    Book class for Scenario3

---
+++ src/Book.java
@@ -0,0 +1,19 @@
+public class Book {
+
+    String title;
+    int pages;
+
+    public Book(String title, int pages) {
+        this.title = title;
+        this.pages = pages;
+    }
+
+    public void addPages(int amount) {
+        pages += amount;
+    }
+
+    public void printBook() {
+        System.out.println("Title: " + title);
+        System.out.println("Pages: " + pages);
+    }
+}
